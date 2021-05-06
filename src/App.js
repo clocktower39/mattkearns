70181e4698aede85ca2d5ece2e7c267ba01fc2cf
clocktower39 <clocktower39@aol.com>
2021-05-05 20:11:17 -0700

    remove image metadata

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Box, CardMedia, Container, Grid, Typography, makeStyles } from '@material-ui/core';
 import { Code, EmojiPeople, Work, Subject } from '@material-ui/icons';
-import img from './img/IMG_1288.jpg';
+import img from './img/IMG_1290.jpg';
 import './App.css';
 
 const useStyle = makeStyles({
@@ -14,13 +14,13 @@ const useStyle = makeStyles({
     color: 'white',
   },
   Box: {
-    height: '150px',
-    width: '150px',
+    height: '125px',
+    width: '125px',
   },
   cardMedia: {
     height: 0,
     paddingTop: '100%',
-    borderRadius: '50%'
+    borderRadius: '50%',
   },
   sidebar: {
     backgroundColor: '#EDEDED',
