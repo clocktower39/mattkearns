50e24d52a3737f7e1badfe0fddedf7f8b2e78278
clocktower39 <clocktower39@aol.com>
2021-05-07 12:41:22 -0700

    work history descriptions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Box, CardMedia, Container, Grid, Typography, makeStyles } from '@material-ui/core';
+import { Avatar, Container, Grid, Typography, makeStyles } from '@material-ui/core';
 import { Code, EmojiPeople, Work, Subject, GitHub, LinkedIn, Instagram } from '@material-ui/icons';
 import img from './img/IMG_1290.jpg';
 import './App.css';
@@ -17,14 +17,12 @@ const useStyle = makeStyles({
     marginTop: '12.5px',
     color: 'white',
   },
-  Box: {
+  TitleHeader:{
+    paddingLeft: '12.5px',
+  },
+  headshot: {
     height: '125px',
     width: '125px',
-  },
-  cardMedia: {
-    height: 0,
-    paddingTop: '100%',
-    borderRadius: '50%',
   },
   sidebar: {
     backgroundColor: '#EDEDED',
@@ -35,6 +33,11 @@ const useStyle = makeStyles({
   },
   mainSubDescriptioin: {
     paddingLeft: '35px',
+    paddingBottom: '25px',
+  },
+  mainSubDescriptioinTasks: {
+    paddingLeft: '35px',
+    paddingBottom: '7.5px',
   },
 })
 
@@ -44,17 +47,16 @@ function App() {
     <Container maxWidth="md" className={classes.root}>
       <Grid container spacing={3} className={classes.gridContainer}>
         <Grid xs={12} item container className={classes.Title}>
+
           <Grid container item xs={4} justify="center">
-            <Box component={"div"} className={classes.Box}>
-              <CardMedia className={classes.cardMedia} image={img} />
-            </Box>
+          <Avatar alt="pic" src={img} className={classes.headshot} />
           </Grid>
 
-          <Grid item xs={8}>
-            <Typography variant="h3" gutterBottom>
+          <Grid item xs={8} className={classes.TitleHeader}>
+            <Typography variant="h4" gutterBottom>
               Matt Kearns
             </Typography>
-            <Typography variant="h5">Full Stack Developer</Typography>
+            <Typography variant="h6">Full Stack Developer</Typography>
           </Grid>
         </Grid>
 
@@ -137,6 +139,7 @@ function App() {
         </Grid>
 
         <Grid container item xs={8} className={classes.main}>
+
           <Typography variant="h5">
             <EmojiPeople /> About Me
           </Typography>
@@ -176,23 +179,48 @@ function App() {
             <Work /> Work Experience
           </Typography>
           <Grid container item xs={12} className={classes.mainSubDescriptioin}>
-            <Grid item xs={12}>
-              <Typography variant="subtitle1">
-                Operation Support Lead
-              </Typography>
+
+            <Typography variant="subtitle1">
+              Operation Support Lead
+            </Typography>
+            <Grid item xs={12} className={classes.mainSubDescriptioinTasks}>
+              <Typography variant="body2">Developed web tools to automate reports for program service level agreements</Typography>
+              <Typography variant="body2">Collaborated with client management team and CVS Chain headquarters Pharmacy representatives via conference calls</Typography>
+              <Typography variant="body2">Liaison between REMS department and IT for program system issues</Typography>
+              <Typography variant="body2">Monitored, coordinated and evaluated program service levels using call management systems to ensure contractual commitments were consistently met</Typography>
+              <Typography variant="body2">Acted as supervisory support to the call center for escalated calls from health care providers, pharmacies, patients and other stakeholders.</Typography>
+              <Typography variant="body2">Identified patterns of performance and system issues then escalated to the appropriate leadership</Typography>
+              <Typography variant="body2">Evaluated unresolved issues and escalated to the appropriate party if necessary</Typography>
+              <Typography variant="body2"></Typography>
             </Grid>
-            <Grid item xs={12}>
-              <Typography variant="subtitle1">
-                REMS Support Representative
-              </Typography>
+
+            <Typography variant="subtitle1">
+              REMS Support Representative
+            </Typography>
+            <Grid item xs={12} className={classes.mainSubDescriptioinTasks}>
+              <Typography variant="body2">Supported multiple REMS programs</Typography>
+              <Typography variant="body2">Answer inbound calls regarding REMS requirements</Typography>
+              <Typography variant="body2">Assist REMS stakeholder with registration, enrollment, certification processing</Typography>
+              <Typography variant="body2">Unlock REMS web accounts and generate temporary password for REMS stakeholders</Typography>
+              <Typography variant="body2">Access REMS stakeholders web account that request web assistance</Typography>
+              <Typography variant="body2">Identify potential adverse events and product complaints to the appropriate REMS drug manufacturer</Typography>
             </Grid>
-            <Grid item xs={12}>
-              <Typography variant="subtitle1">Ranch Hand</Typography>
+
+            <Typography variant="subtitle1">Ranch Hand</Typography>
+            <Grid item xs={12} className={classes.mainSubDescriptioinTasks}>
+              <Typography variant="body2">Supervised guest safety during river interactions and ranch stays</Typography>
+              <Typography variant="body2">Documented and reported any property damage or customer concerns and completed requested services to resolve</Typography>
+              <Typography variant="body2">Maintained equipment and ranch structures</Typography>
             </Grid>
-            <Grid item xs={12}>
-              <Typography variant="subtitle1">
-                Initial Loss Reporting Representative
-              </Typography>
+            
+            <Typography variant="subtitle1">
+              Initial Loss Reporting Representative
+            </Typography>
+            <Grid item xs={12} className={classes.mainSubDescriptioinTasks}>
+              <Typography variant="body2">Communicate with customers and associates by phone, mobile app, click-to-chat and Internet reporting</Typography>
+              <Typography variant="body2">Gather and document loss information</Typography>
+              <Typography variant="body2">Handle a large volume of calls in a collaborative team setting</Typography>
+              <Typography variant="body2">Use State Farm systems and technology</Typography>
             </Grid>
           </Grid>
 
@@ -200,20 +228,12 @@ function App() {
             <Subject /> Education
           </Typography>
           <Grid container item className={classes.mainSubDescriptioin}>
-            <Grid item xs={12}>
               <Typography variant="subtitle1">FreeCodeCamp</Typography>
-              <Typography variant="body2">
-                • Responsive Web Design Certification
-              </Typography>
-              <Typography variant="body2">
-                • JavaScript Algorithms and Data Structures Certification
-              </Typography>
-              <Typography variant="body2">
-                • Front End Libraries Certification
-              </Typography>
-              <Typography variant="body2">
-                • Data Visualization Certification
-              </Typography>
+            <Grid item xs={12} className={classes.mainSubDescriptioinTasks}>
+              <Typography variant="body2">Responsive Web Design Certification</Typography>
+              <Typography variant="body2">JavaScript Algorithms and Data Structures Certification</Typography>
+              <Typography variant="body2">Front End Libraries Certification</Typography>
+              <Typography variant="body2">Data Visualization Certification</Typography>
             </Grid>
             <Grid item xs={12}>
               <Typography variant="subtitle1">
