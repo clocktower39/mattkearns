54e3e996415d2ed8540b8a0c4548adb3b94a4109
clocktower39 <clocktower39@aol.com>
2021-12-03 23:54:58 -0700

    swap image and about, svg text path movement, background color changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,31 +1,28 @@
-import React from "react";
-import {
-  Avatar,
-  Container,
-  Grid,
-  IconButton,
-  Paper,
-  Typography,
-} from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { ExpandMore, LinkedIn, GitHub, Instagram } from '@mui/icons-material';
+import React, { useEffect, useRef } from "react";
+import { Avatar, Container, Grid, IconButton, Paper, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { ExpandMore, LinkedIn, GitHub, Instagram } from "@mui/icons-material";
+import AOS from 'aos';
 import ProjectCard from "./ProjectCard";
 import FavoritesList from "./FavoritesList";
 import Logos from "./Logos";
 import headshot from "../img/IMG_1290.jpg";
+// import codeBracketsSVG from "../img/codeBracketsSVG.svg";
+// import consoleSVG from "../img/consoleSVG.svg";
 import { projects, list } from "../states";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: "100vh",
+    height: "100%",
     backgroundColor: "#5927E5",
     color: "#deeaef",
+    backgroundImage: "linear-gradient(176deg, #22313F 70%, #F4F4F4 calc(70% + 2px))",
   },
   introTypography: {
     fontFamily: "Odibee Sans, cursive",
   },
-  headshotContainer:{
-    justifyContent: 'center',
+  headshotContainer: {
+    justifyContent: "center",
   },
   headshot: {
     border: "3px solid white",
@@ -34,7 +31,7 @@ const useStyles = makeStyles((theme) => ({
     color: "#50fbdf",
   },
   section: {
-    height: "100vh",
+    height: "100%",
   },
   sectionTitle: {
     // color: "#5927E5",
@@ -58,6 +55,8 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Home() {
   const classes = useStyles();
+  const textContainerRef = useRef(null);
+  const textPathRef = useRef(null);
 
   const handleSocialLink = (type) => {
     switch (type) {
@@ -65,8 +64,7 @@ export default function Home() {
         window.location.href = "https://github.com/clocktower39";
         break;
       case "linkedIn":
-        window.location.href =
-          "https://www.linkedin.com/in/matthew-kearns-6b8865117/";
+        window.location.href = "https://www.linkedin.com/in/matthew-kearns-6b8865117/";
         break;
       case "instagram":
         window.location.href = "https://www.instagram.com/kearns39/";
@@ -75,12 +73,34 @@ export default function Home() {
         break;
     }
   };
+
+  const updateTextPathOffset = (offset) => {
+    textPathRef.current.setAttribute('startOffset', offset);
+  }
+
+  const onScrollText = () => {
+    updateTextPathOffset(window.scrollY*1.2);
+  }
+
+  useEffect(()=>{
+    AOS.init({ duration: 1000 });
+    window.addEventListener('scroll', onScrollText)
+    // eslint-disable-next-line
+  },[])
+
   return (
     <div className={classes.root}>
       <Container maxWidth="md" className={classes.section}>
         <Grid container style={{ height: "100%" }}>
-          <Grid container style={{justifyContent: "center", alignItems: "center"}} >
-            <Grid item container style={{justifyContent: "center"}} xs={12}></Grid>
+          <Grid container style={{ justifyContent: "space-around", alignItems: "center" }}>
+            <Grid className={classes.headshotContainer} container item xs={12} sm={4}>
+              <Avatar
+                alt="pic"
+                src={headshot}
+                className={classes.headshot}
+                sx={{ width: 175, height: 175 }}
+              />
+            </Grid>
             <Grid container item xs={6}>
               <Grid item xs={12}>
                 <Typography
@@ -92,43 +112,48 @@ export default function Home() {
                 </Typography>
               </Grid>
               <Grid item xs={12}>
-                <Typography
-                  display="inline"
-                  variant="h3"
-                  className={classes.introTypography}
-                >
+                <Typography display="inline" variant="h3" className={classes.introTypography}>
                   Matt Kearns.
                 </Typography>
               </Grid>
               <Grid item xs={12}>
-                <Typography
-                  display="inline"
-                  variant="h6"
-                  className={classes.introTypography}
-                >
-                  I'm a self taught{" "}
-                  <span className={classes.accent}>full stack developer</span>.
-                  passionate hacker and love to break everything down...even if
-                  I end up just plain breaking it.
+                <Typography display="inline" variant="h6" className={classes.introTypography}>
+                  I'm a self taught <span className={classes.accent}>full stack developer</span>.
+                  passionate hacker and love to break everything down...even if I end up just plain
+                  breaking it.
                 </Typography>
               </Grid>
             </Grid>
-            <Grid className={classes.headshotContainer} container item xs={12} sm={4}>
-              <Avatar alt="pic" src={headshot} className={classes.headshot} sx={{ width: 175, height: 175 }} />
-            </Grid>
-            <Grid item container style={{justifyContent: "center"}} xs={12}>
+            {/* <Grid item container style={{ justifyContent: "center" }} xs={12}>
+              <Grid item xs={6}>
+                <img src={codeBracketsSVG} style={{height: '80%', width: '80%',}} />
+              </Grid>
+              <Grid item xs={6}>
+                <img src={consoleSVG} style={{height: '80%', width: '80%',}} />
+              </Grid>
+            </Grid> */}
+            <Grid item container style={{ justifyContent: "center" }} xs={12}>
               <ExpandMore
                 className={classes.ExpandMore}
-                style={{ fontSize: "115px" }}
+                style={{ fontSize: "115px", color: "#000000", paddingTop: "125px" }}
               />
             </Grid>
+            
+            <svg id="text-container" ref={textContainerRef} viewbox="0 0 1500 200" xmlns="http://ww.w3.org/2000/svg">
+              <path id="text-curve" d="m0 100s269.931 86.612 520 0c250.069-86.612 480 0 480 0" fill="none" />
+              <text y="40" font-size="56">
+                <textPath href="#text-curve" id="text-path" ref={textPathRef}>
+                  Lets code!
+                </textPath>
+              </text>
+            </svg>
           </Grid>
         </Grid>
       </Container>
 
-      <div style={{ backgroundColor: "#008B74" }}>
+      <div style={{ background: "linear-gradient(#F4F4F4, #5D5B7D)", }}>
         <Container maxWidth="md">
-          <Typography variant="h3" className={classes.sectionTitle}>
+          <Typography variant="h3" className={classes.sectionTitle} style={{color: '#000'}}>
             Projects
           </Typography>
           <Grid container spacing={3}>
@@ -141,17 +166,17 @@ export default function Home() {
 
       <div
         style={{
-          background: "linear-gradient(#008B74, #5927E5)",
+          background: "linear-gradient(#5D5B7D, #282744)",
           padding: "100px 0",
         }}
       >
-      <Container maxWidth="md">
-        <Paper>
-        <Grid container spacing={2}>
-          <Logos />
-        </Grid>
-        </Paper>
-      </Container>
+        <Container maxWidth="md">
+          <Paper>
+            <Grid container spacing={2}>
+              <Logos />
+            </Grid>
+          </Paper>
+        </Container>
         <Container maxWidth="md">
           <Typography variant="h3" className={classes.sectionTitle}>
             Favorite Books, Movies, & Video Games
