7b6139ff209431493c04880b635493ce690e4063
clocktower39 <clocktower39@aol.com>
2025-02-14 23:54:35 -0700

    config rollup for chunking

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom"],
+          "mui-vendor": [
+            "@mui/material",
+            "@mui/icons-material",
+            "@emotion/react",
+            "@emotion/styled",
+          ],
+          particles: ["@tsparticles/plugin-polygon-mask", "@tsparticles/react", "tsparticles"],
+          utils: ["pathseg", "react-google-recaptcha", "react-photo-gallery", "react-terminal-ui"],
+        },
+      },
+    },
+  },
+});
