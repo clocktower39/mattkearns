15ab5398479ebed62cacbf241588d7e3433acf5e
Matt <clocktower39@aol.com>
2026-03-13 00:23:43 -0700

    vite 8 adjustment

--- vite.config.ts
+++ vite.config.ts
@@ -7,16 +7,36 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          "react-vendor": ["react", "react-dom"],
-          "mui-vendor": [
-            "@mui/material",
-            "@mui/icons-material",
-            "@emotion/react",
-            "@emotion/styled",
-          ],
-          particles: ["@tsparticles/plugin-polygon-mask", "@tsparticles/react", "tsparticles"],
-          utils: ["pathseg", "react-google-recaptcha", "react-photo-gallery", "react-terminal-ui"],
+        manualChunks(id) {
+          if (!id.includes("node_modules")) return;
+
+          if (id.includes("react") || id.includes("react-dom")) return "react-vendor";
+
+          if (
+            id.includes("@mui/material") ||
+            id.includes("@mui/icons-material") ||
+            id.includes("@emotion/react") ||
+            id.includes("@emotion/styled")
+          ) {
+            return "mui-vendor";
+          }
+
+          if (
+            id.includes("@tsparticles/plugin-polygon-mask") ||
+            id.includes("@tsparticles/react") ||
+            id.includes("tsparticles")
+          ) {
+            return "particles";
+          }
+
+          if (
+            id.includes("pathseg") ||
+            id.includes("react-google-recaptcha") ||
+            id.includes("react-photo-gallery") ||
+            id.includes("react-terminal-ui")
+          ) {
+            return "utils";
+          }
         },
       },
     },
